71aaee6fc5b6864e6229dc3239a15ac01e3d4a7a
cupang-afk <pixelview56@gmail.com>
2024-01-28 19:33:32 +0700

    move log so that only log the new plugin

--- cupang_updater/task/scan.py
+++ cupang_updater/task/scan.py
@@ -76,7 +76,6 @@ def scan_plugins(config: Config) -> None | Any:
             name, version, authors = jar_info(jar)
             default_plugin_data = updater_manager.get_plugin_default()
 
-            log.info(f"[green]Update config for {name} [cyan]{jar.name}")
             if plugins_config.get(name, sy.YAML(None, sy.EmptyNone())).data is not None:
                 if (
                     hash.md5() == plugins_config[name]["hashes"]["md5"].data
@@ -93,6 +92,7 @@ def scan_plugins(config: Config) -> None | Any:
             #
             # if the config for plugin is exists then updating will be slow (cuz the validation stuff)
             # otherwise it will be fast
+            log.info(f"[green]Update config for {name} [cyan]{jar.name}")
             if not plugins_config.get(name, sy.YAML(None, sy.EmptyNone())).data:
                 plugin_data: sy.YAML = default_plugin_data
             else:
